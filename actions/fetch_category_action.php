1915dcf8f161338755f0e89e0d148fe1f825cfbd
Lamiokorr <138886889+Lamiokorr@users.noreply.github.com>
2025-10-09 11:55:33 +0000

    Afresh

--- actions/fetch_category_action.php
+++ actions/fetch_category_action.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $customer_id = $_SESSION['customer_id'];
 
 
-    $category = get_all_categories_ctr($category_id);
+    $category = get_all_categories_ctr();
 
     if($category){
          echo json_encode($category);  
