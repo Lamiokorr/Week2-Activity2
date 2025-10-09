94ce68fd5c6dad2a4252c22ff4b8368c907d737e
Lamiokorr <138886889+Lamiokorr@users.noreply.github.com>
2025-10-09 11:52:18 +0000

    Afresh

--- actions/add_category_action.php
+++ actions/add_category_action.php
@@ -6,7 +6,7 @@ session_start();
 
 require_once '../controllers/category_controller.php';
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $category_name = $_POST['cat_name'];
+    $category_name = $_POST['name'];
     $customer_id = $_SESSION['customer_id'];
 
     $result = add_category_ctr($category_name, $customer_id);
