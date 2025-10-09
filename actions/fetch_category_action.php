83765b089ed42d32d0574909b2b8f41fef0f490e
Lamiokorr <138886889+Lamiokorr@users.noreply.github.com>
2025-10-09 12:26:37 +0000

    Afresh

--- actions/fetch_category_action.php
+++ actions/fetch_category_action.php
@@ -4,18 +4,11 @@ header('Content-Type: application/json');
 
 session_start();
 require_once '../controllers/category_controller.php';
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $customer_id = $_SESSION['customer_id'];
 
+$category = get_all_categories_ctr();
 
-    $category = get_all_categories_ctr();
-
-    if($category){
-         echo json_encode($category);  
-        } else {
-            echo json_encode("error");  
-    }
-
+if ($category) {
+    echo json_encode($category);
+} else {
+    echo json_encode([]);
 }
-
-?>
