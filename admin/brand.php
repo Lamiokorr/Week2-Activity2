9e20dd045c6cad340f66cf909ace4a2e11983fe2
Lamiokorr <138886889+Lamiokorr@users.noreply.github.com>
2025-10-25 20:20:11 +0000

    Afresh

--- admin/brand.php
+++ admin/brand.php
@@ -1,5 +1,11 @@
 <?php
 require_once("../settings/core.php");
+require_once("../controllers/brand_controller.php");
+
+if (!isLoggedIn() || !isAdmin()) {
+    header("Location: ../login/login.php");
+    exit();
+}
 
 
 
